d531cdf485d78cf6073ba5f4e90ed2b3ccdb8b58
spjy <spjy@hawaii.edu>
2019-06-25 23:24:04 -1000

    feat: remove frequency for portable

--- src/renderer/pages/Portable.vue
+++ src/renderer/pages/Portable.vue
@@ -3,49 +3,33 @@
     <Navbar page="Portable Radio" />
     <div class="flex flex-col p-6">
       <div
-        :class="`w-full text-center shadow-lg rounded-lg block p-4 mb-6`"
+        :class="`flex flex-col md:flex-row justify-center w-full text-center shadow rounded-lg p-4 mb-4`"
       >
         <div
-          :class="`${selecting ? 'select-active bg-gray-100' : ''} cursor-pointer text-center bg-white hover:bg-gray-100 text-gray-800 font-semibold border border-gray-400 rounded py-1 px-4 inline m-1`"
+          :class="`${tx ? 'select-active bg-gray-200' : ''} cursor-pointer text-center bg-white hover:bg-gray-200 text-gray-800 font-semibold border border-gray-400 rounded p-2 mx-2 my-2 inline`"
+          @click="onTx"
         >
-          Tx
+          Transmit
         </div>
         <div
-          :class="`${active ? 'action-active bg-red-600 text-white' : 'text-gray-800'} cursor-pointer text-center bg-red-100 hover:bg-red-600 hover:text-white font-semibold border border-gray-400 rounded py-1 px-4 inline m-1`"
+          :class="`${emergency ? 'action-active bg-red-600 text-white' : 'text-gray-800'} cursor-pointer text-center bg-red-100 hover:bg-red-600 hover:text-white font-semibold border border-gray-400 rounded p-2 mx-2 my-2 inline`"
+          @click="onEmergency"
         >
           Emergency
         </div>
       </div>
-      <div class="flex">
-        <div
-          :class="`w-1/2 text-center shadow-lg p-4 mt-2 mr-2 mb-2 rounded-lg inline-block relative`"
-        >
-          <SectionTitle title="Frequency" />
-          <div class="flex justify-between w-full bg-white border text-lg border-gray-300 text-gray-700 py-3 px-4 pr-8 rounded leading-tight">
-            <div class="text-3xl text-blue-600 cursor-pointer select-none">
-              &#8592;
-            </div>
-            <div class="pt-2">
-              115.9 MHz
-            </div>
-            <div class="text-3xl text-blue-600 cursor-pointer select-none">
-              &#8594;
-            </div>
-          </div>
-        </div>
-        <div
-          :class="`w-1/2 text-center shadow-lg p-4 mt-2 mr-2 mb-2 rounded-lg inline-block relative`"
-        >
-          <SectionTitle title="Channel" />
-          <div class="relative">
-            <select class="block appearance-none w-full bg-white border text-lg border-gray-300 text-gray-700 py-5 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-              <option>Police South</option>
-              <option>Police North</option>
-              <option>Opts 1</option>
-            </select>
-            <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-              <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
-            </div>
+      <div
+        :class="`w-full text-center shadow p-4 mt-2 mr-2 mb-2 rounded-lg inline-block relative`"
+      >
+        <SectionTitle title="Channel" />
+        <div class="relative">
+          <select class="block appearance-none w-full bg-white border border-gray-300 text-gray-700 py-4 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+            <option>Police South</option>
+            <option>Police North</option>
+            <option>Opts 1</option>
+          </select>
+          <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
           </div>
         </div>
       </div>
@@ -62,8 +46,47 @@ export default {
     Navbar,
     SectionTitle,
   },
+  data() {
+    return {
+      tx: false,
+      emergency: false,
+    };
+  },
+  methods: {
+    onTx() {
+      this.tx = !this.tx;
+    },
+    onEmergency() {
+      this.emergency = !this.emergency;
+    },
+  },
 };
 </script>
 
 <style>
+.select-active {
+  animation: shadow-pulse-select 1s infinite;
+}
+
+.action-active {
+  animation: shadow-pulse-action 1s infinite;
+}
+
+@keyframes shadow-pulse-select {
+  0% {
+    box-shadow: 0 0 0 0px rgba(0, 0, 0, 0.2);
+  }
+  100% {
+    box-shadow: 0 0 0 7px rgba(0, 0, 0, 0);
+  }
+}
+
+@keyframes shadow-pulse-action {
+  0% {
+    box-shadow: 0 0 0 0px rgba(192, 18, 18, 0.5);
+  }
+  100% {
+    box-shadow: 0 0 0 7px rgba(179, 67, 67, 0);
+  }
+}
 </style>
